ddd3725fabb87da8e7d83600d5d87049bf908fd7
Craig O'Donnell <CODonnell@vhb.com>
2022-01-13 08:54:16 -0500

    comments

--- slice.go
+++ slice.go
@@ -3,6 +3,7 @@ package slice
 type Insertable []int
 
 // InsertValueAtIndex inserts a value at the specified index
+// and returns the new slice
 func (a Insertable) InsertValueAtIndex(value int, index int) Insertable {
 	a = append(a[:index+1], a[index:]...)
 	a[index] = value
